68cf87ad5f465cf0e5612ab6fa34722b67bdc21d
Dhruv Gupta <guptadhruv51@gmail.com>
2024-06-20 13:09:57 +1000

    Update and Create APIs added

---
+++ minorproject1/src/main/java/com/example/minorproject1/dtos/CreateStudentRequest.java
@@ -0,0 +1,29 @@
+package com.example.minorproject1.dtos;
+
+import com.example.minorproject1.models.Student;
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CreateStudentRequest
+{
+    private String name;
+
+    private String email;
+    @NotBlank
+    private String mobile;
+   public Student to()
+           //dto to entity
+   {
+       return Student.builder()
+               .name(this.name)
+               .email(this.email)
+               .mobile(this.mobile)
+               .build();
+   }
+}
