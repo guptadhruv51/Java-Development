6ef20b1db253d16d6e7614ca747461e16a5d8065
Dhruv Gupta <guptadhruv51@gmail.com>
2024-06-25 14:17:59 +1000

    Unit Testing started

--- minorproject1/src/test/java/com/example/minorproject1/Minorproject1ApplicationTests.java
+++ minorproject1/src/test/java/com/example/minorproject1/Minorproject1ApplicationTests.java
@@ -1,12 +1,12 @@
 package com.example.minorproject1;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class Minorproject1ApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
---
+++ minorproject1/src/test/java/com/example/minorproject1/TransactionServiceTest.java
@@ -0,0 +1,9 @@
+package com.example.minorproject1;
+
+public class TransactionServiceTest
+{
+
+    /**
+     * Skip functions which do not need to be tested
+     */
+}
