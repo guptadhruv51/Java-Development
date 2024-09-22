93c25918cfc007ab5b982cd096ed2cf8549786e0
Dhruv Gupta <guptadhruv51@gmail.com>
2024-09-23 08:41:24 +1000

    H-2 DB added

--- E-Commerce/src/main/java/com/example/ecommerce/E_Commerce/Models/Category.java
+++ E-Commerce/src/main/java/com/example/ecommerce/E_Commerce/Models/Category.java
@@ -1,16 +1,25 @@
 package com.example.ecommerce.E_Commerce.Models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-//@Entity
+@Entity(name="Categories")
 public class Category
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long categoryId;
     private String categoryName;
 
     public Category(Long categoryId, String categoryName) {
         this.categoryId = categoryId;
         this.categoryName = categoryName;
+    }
+
+    public Category() {
+
     }
 
     public Long getCategoryId() {
