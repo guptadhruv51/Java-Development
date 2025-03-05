ce2aaaad9dd2f9d29155dfbf4757280bae3341e8
Dhruv Gupta <guptadhruv51@gmail.com>
2025-03-05 19:12:20 +1100

    Swagger UI added

--- expense-tracker-app/src/main/java/com/example/expense_tracker_app/ExpenseTrackerAppApplication.java
+++ expense-tracker-app/src/main/java/com/example/expense_tracker_app/ExpenseTrackerAppApplication.java
@@ -1,8 +1,22 @@
 package com.example.expense_tracker_app;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Expense Tracker REST API documentation",
+				description="Expense Tracker REST API documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name="Dhruv Gupta",
+						email="guptadhruv51@gmail.com"
+				)
+		)
+)
 @SpringBootApplication
 public class ExpenseTrackerAppApplication {
 
